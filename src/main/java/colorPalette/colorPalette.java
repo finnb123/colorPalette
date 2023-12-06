b8ffd1924331723a013984915ee26337a489894c
brend <121110218@umail.ucc.ie>
2023-12-06 17:01:53 +0000

    Added IllegalArgumentException thrown when no parameter passed to colorPalette constructor

--- src/main/java/colorPalette/colorPalette.java
+++ src/main/java/colorPalette/colorPalette.java
@@ -3,7 +3,9 @@ package colorPalette;
 public class colorPalette {
 
     int[][] palette;
-
+    public colorPalette() throws IllegalArgumentException{
+        throw new IllegalArgumentException("Expected 1 parameter, but 0 were provided");
+    }
     public colorPalette(int numberOfColors){
         this.palette = new int[][]{
                 {0, 0, 0},
