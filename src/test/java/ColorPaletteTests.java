44e965a7583aadbcd8924262f846cb6224f8f70c
brend <121110218@umail.ucc.ie>
2023-12-06 16:56:56 +0000

    Added test for constructor with 0 parameters

--- src/test/java/ColorPaletteTests.java
+++ src/test/java/ColorPaletteTests.java
@@ -1,17 +1,25 @@
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import colorPalette.colorPalette;
-import org.junit.jupiter.api.Assertions;
+
 
 public class ColorPaletteTests{
     @Test
     @DisplayName("Testing colorPalette object initialization")
-    public void initializeObject(){
+    public void initializeObjectWithTwo(){
         colorPalette colorTwo = new colorPalette(2);
         int[][] expectedPalette = {
                 {0,0,0},
                 {255,255,255}
         };
         Assertions.assertArrayEquals(expectedPalette, colorTwo.getPalette());
+    }
+
+    @Test
+    @DisplayName("Testing if constructor with no input results in exception")
+    public void testColorPaletteNoInput_throwsIllegalArgumentException(){
+        IllegalArgumentException thrown = Assertions.assertThrows( IllegalArgumentException.class, () -> new colorPalette());
+        Assertions.assertEquals("Expected 1 parameter, but 0 were provided", thrown.getMessage());
     }
 }
