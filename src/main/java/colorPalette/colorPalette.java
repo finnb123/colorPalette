df4300b99215ffc4b8fb746202bde3c61a2d8338
brend <121110218@umail.ucc.ie>
2023-12-06 17:28:17 +0000

    Added error handling for constructor if illegal arguments passed

--- src/main/java/colorPalette/colorPalette.java
+++ src/main/java/colorPalette/colorPalette.java
@@ -6,7 +6,10 @@ public class colorPalette {
     public colorPalette() throws IllegalArgumentException{
         throw new IllegalArgumentException("Expected 1 parameter, but 0 were provided");
     }
-    public colorPalette(int numberOfColors){
+    public colorPalette(int numberOfColors) throws IllegalArgumentException{
+        if (numberOfColors < 2 || numberOfColors > 1025 || !((numberOfColors & (numberOfColors - 1)) == 0)){
+            throw new IllegalArgumentException("Input must be greater than 1, a power of 2, and less than 1025.");
+        }
         this.palette = new int[][]{
                 {0, 0, 0},
                 {255, 255, 255}
