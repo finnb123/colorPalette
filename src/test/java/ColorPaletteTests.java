3812eed53638daccfc56be3eedc367a2e5cea841
brend <121110218@umail.ucc.ie>
2023-12-06 15:37:40 +0000

    Added test for constructor of colorPalette involving 2 colors

--- src/test/java/ColorPaletteTests.java
+++ src/test/java/ColorPaletteTests.java
@@ -1,10 +1,20 @@
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import colorPalette.colorPalette;
+import org.junit.jupiter.api.Assertions;
 
 public class ColorPaletteTests{
     @Test
     @DisplayName("Testing colorPalette object initialization")
     public void initializeObject(){
+        colorPalette colorTwo = new colorPalette(2);
+        int[][] expectedPalette = {
+                {0,0,0},
+                {255,255,255}
+        };
+        Assertions.assertEquals(expectedPalette, colorTwo);
+
+
 
     }
 }
